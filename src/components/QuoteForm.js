52ab4c94bda253e699174801c607358523f970b0
github username <github email address>
2019-11-02 09:59:23 -0400

    Done.

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -6,32 +6,46 @@ import { addQuote } from '../actions/quotes';
 class QuoteForm extends Component {
 
   state = {
-    //set up a controlled form with internal state
+    author: "",
+    content: ""
   }
 
   handleOnChange = event => {
-    // Handle Updating Component State
+    const {name, value} = event.target
+    this.setState({
+      [name]: value
+    })
   }
 
   handleOnSubmit = event => {
-    // Handle Form Submit event default
-    // Create quote object from state
-    // Pass quote object to action creator
-    // Update component state to return to default state
+    event.preventDefault()
+    const quote = {
+      ...this.state,
+      id: uuid(),
+      votes: 0
+    }
+    this.props.addQuote(quote)
+    this.setState({
+      author: "",
+      content: ""
+    })
   }
 
   render() {
+    console.log("quoteFormState:", this.state)
     return (
       <div className="container">
         <div className="row">
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form onSubmit={this.handleOnSubmit}
+                  className="form-horizontal">
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
-                      <textarea
+                      <textarea onChange={this.handleOnChange}
+                        name="content"
                         className="form-control"
                         value={this.state.content}
                       />
@@ -40,7 +54,8 @@ class QuoteForm extends Component {
                   <div className="form-group">
                     <label htmlFor="author" className="col-md-4 control-label">Author</label>
                     <div className="col-md-5">
-                      <input
+                      <input onChange={this.handleOnChange}
+                        name="author"
                         className="form-control"
                         type="text"
                         value={this.state.author}
@@ -49,7 +64,7 @@ class QuoteForm extends Component {
                   </div>
                   <div className="form-group">
                     <div className="col-md-6 col-md-offset-4">
-                      <button type="submit" className="btn btn-default">Add</button>
+                      <button type="submit" className="btn btn-default btn-danger">Add</button>
                     </div>
                   </div>
                 </form>
@@ -62,5 +77,10 @@ class QuoteForm extends Component {
   }
 }
 
-//add arguments to connect as needed
-export default connect()(QuoteForm);
+const mapDispatchToProps = dispatch => {
+  return {
+    addQuote: quote => dispatch(addQuote(quote))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(QuoteForm);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,28 @@
 export default (state = [], action) => {
-  return state;
+  console.log("reducer Action", action)
+  console.log("reducer begin state", state)
+
+  let idx
+  let updatedQuote
+  switch (action.type) {
+    case 'ADD_QUOTE':
+      return [...state, action.quote]
+    case 'REMOVE_QUOTE':
+      const newArr = state.filter(quote => quote.id !== action.quoteId)
+      return newArr
+    case 'UPVOTE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      updatedQuote = {...state[idx]}
+
+      updatedQuote.votes = updatedQuote.votes + 1
+      return [...state.slice(0, idx), updatedQuote, ...state.slice(idx +1, state.length)]
+    case 'DOWNVOTE_QUOTE':
+      idx = state.findIndex(quote => quote.id === action.quoteId)
+      updatedQuote = {...state[idx]}
+
+      updatedQuote.votes = (updatedQuote.votes > 0 ? updatedQuote.votes - 1 : 0)
+      return [...state.slice(0, idx), updatedQuote, ...state.slice(idx +1, state.length)]
+    default:
+      return state
+  }
 }
